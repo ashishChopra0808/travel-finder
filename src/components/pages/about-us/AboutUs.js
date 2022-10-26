b1f79be0465fb3279306e67860fa64052f840679
Ashish Chopra <ashish.chopra@aquera.com>
2022-10-26 12:50:19 +0530

    first commit

---
+++ src/components/pages/about-us/AboutUs.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import BreadCrumb from "./BreadCrumb";
+import AboutWrapper from "./AboutWrapper";
+import GuideWrapper from "./GuideWrapper";
+import Achievement from "./Achievement";
+
+class AboutUs extends Component {
+  render() {
+    return (
+       <>
+          <BreadCrumb/>
+          <AboutWrapper/>
+          <GuideWrapper/>
+          <Achievement/>
+       </>
+    );
+  }
+}
+
+export default AboutUs;
---
+++ src/json/PackageDetails.js
@@ -0,0 +1,76 @@
+export const packagesData = [
+    {
+        title: '3 Nights / 4 Days Itinerary For Serbia',
+        slug: '3n-4d-itinerary-for-serbia',
+        daysDetails: [
+            {
+                title: 'Day 1: ARRIVAL AT BELGRADE, DAY AT LEISURE',
+                description: 'Arrival in Belgrade, Transfer to the hotel. Day at leisure. Please make yourself comfortable at hotel'
+            },
+            {
+                title: 'Day 2: THE BELGRADE CITY TOUR',
+                description: 'After Breakfast, the Belgrade tour. Explore Belgrade\'s history, culture, architecture, lifestyle, and everything that you might like to know to get around. visit the Old Town and New Belgrade; Our journey begins from the most important place: between The National Assembly from one, and The City Assembly from the other side, and then we will pass through one of the main streets of Belgrade, named Kneza Miloša, with beautiful architectural style from 20th century. Driving through the luxury area of the city - Dedinje, you will see The House of Flowers (Mausoleum of the most famous president in Ex Yugoslavia, Tito), then we will pass by the Royal Compound, and the most luxury street with residence of many embassies.'
+            },
+            {
+                title: 'Day 3: NOVI SAD & SREMSKI KARLOVCI TOUR',
+                description: 'After breakfast you can join us on a tour of the fascinating and culturally diverse city of Novi Sad and discover its rich history and heritage, before making a visit to nearby Sremski Karlovci. Reaching Novi Sad, we will be able to admire breathtaking views of the city and explore the Zmaj Jovina street, Freedom Square and the National Theater. We will then set out to discover the many crafts that are preserved in the magnificent Petrovaradin fortress, before heading to the nearby town of Sremski Karlovci, to indulge in a combination of history, religion, honey and beauty.'
+            },
+            {
+                title: 'Day 4: DEPARTURE FROM BELGRADE',
+                description: 'Breakfast at hotel and Ready to Say Goodbye to Belgrade.'
+            }
+        ]
+    },
+    {
+        title: '3 Nights / 4 Days Itinerary For Georgia',
+        slug: '3n-4d-itinerary-for-georgia',
+        daysDetails: [
+            {
+                title: 'Day 1: ARRIVAL AT TBLISI, DAY AT LEISURE',
+                description: 'Arrival in Tbilisi, Transfer to the hotel. Day at leisure. Please make yourself comfortable at hotel'
+            },
+            {
+                title: 'Day 2: THE TBLISI CITY TOUR',
+                description: 'Experience Tbilisi at its best by exploring the city\'s rich historical and cultural heritage, modern life and exciting attractions, as we delve into the lifestyle and culture of its people and enjoy their delicious cuisine in the background of a wonderful natural environment. We start with a visit to the Metekhi Temple and the Peace Bridge, before heading to Chardin Street in the old city, with its ancient architecture and sulfur baths. We continue our journey by taking the cable car to Narikala Fort and the funicular to Mtatsminda mountain, the highest spot in the whole city. While you\'re there, enjoy the attractions of the mountain amusement park and grab a quick bite at any of the local restaurants (at your own expense).'
+            },
+            {
+                title: 'Day 3: ANNANURI GUDAURI KAZBEGI TOUR',
+                description: 'After breakfast you can join us on a tour of the fascinating and culturally e Georgian Military Road and the beautiful Ananuri Castle and Church. The first stop offers an interesting panorama over the Zhinvali reservoir. As we continue towards the scenic Pass, we will come across the beautiful ski resort of Gudauri, resting a little closer to the summit, at 2395 meters. Stepantsminda. While you are there, you can go on a jeep tour that will take you to the Gergeti temple and, for an additional fee, you can also choose to visit Dariali valley where you can go on a full river cruise.'
+            },
+            {
+                title: 'Day 4: DEPARTURE FROM TBLISI',
+                description: 'Breakfast at hotel and Ready to Say Goodbye to TBLISI'
+            }
+        ]
+    },
+    {
+        title: 'Georgia Winter Special 5 Nights / 6 Days',
+        slug: 'georgia-winter-special-5n-6d',
+        daysDetails: [
+            {
+                title: 'Day 1: Arrival',
+                description: 'Arrive at the Kutaisi international Airport at 20:00. Transfer to the city and check-in the hotel. Rest and relax.<br/><b>Overnight in Tskaltubo or in Kutaisi</b>'
+            },
+            {
+                title: 'Day 2: From Kutaisi to Uplistikhe, Ananuri and Gudauri',
+                description: 'Breakfast at the hotel; on this day we will move to Easter Georgia. Our first stop will be the hometown of JOSEPH STALIN. Visit the museum of Stalin in Gori. Later, visit Uplistsikhe caved town - spectacular town with mysterious history from III-II millennium B.C. Then, we will drive to Ananuri Complex and after surveying the beautiful area, move to Gudauri through the main military road of Georgia. Check-in the hotel there and rest after the long drive.<br/><b>Overnight in Gudauri</b>'
+            },
+            {
+                title: 'Day 3: Tour to Kazbegi & Dariali',
+                description: 'Breakfast at the hotel. On this day, if weather condition permits, we will have the tour in one of the most popular touristic destination in Georgia - Kazbegi. First, we will drive to Kazbegi and change the transport to 4x4 vehicle to ride up on the top of mountain and visit the Gergeti Monastery, which is surrounded by huge mountains of mysterious Caucasus. Later, we will drive to Dariali Gorge, which is most important historical center of country - main border of Georgia and Russia. On the way back to Gudauri, upon request, we will drive to Sno Village and survey the surrounding. Returning to the hotel in Gudauri and rest. <br/>If weather condition not permits, you will have the free day in Gudauri to enjoy the snow activities by yourself.<br/><b>Overnight in Gudauri</b>'
+            },
+            {
+                title: 'Day 4: From Gudauri to Mtskheta and Tbilisi',
+                description: 'Breakfast at the hotel. Free morning in Gudauri to enjoy the picturesque area. Check out at 11:30 AM and moving to Capital City - Tbilisi. On the way visit the ancient capital of Georgia - Mtskheta. whole city is in the list of UNESCO World Heritage. The mast see places are - Jvari Monastery and Svetitkhoveli Cathedral. Then, drive to Tbilisi and time for shpping at Tbilisi Mall, Orbeliani Bazaar and Flea Market. Check-in the hotel and rest. Walking on Rustaveli Avenue. The evening will be free for your own.<br/><b>Overnight in Tbilisi</b>'
+            },
+            {
+                title: 'Day 5: Tbilisi City Tour',
+                description: 'Breakfast at the hotel. In the morning, we will drive to the OLD TBILISI and start the walking tour to discover the beauty of ancient capital - walking tour starts at the Europe Square and includes: Metehki Church, Cable Car to the Narikala fortress (ride up by cable car), “Mother of Kartli”, walking down to Abano District. Then continue walking to Legvtakhevi waterfall. Free time there for photo shooting. Visiting Trinity Cathedarl.<br/>At the evening, we will ride to Mtatsminda Amusement Park by Tram. The park is highest point of the city. There you can find the good viewpoints to whole city. Free time there for refresh and walking. After, we will ride back to our transport by Tram again. Drop off at the hotel and free time.<br/><b>Overnight in Tbilisi</b>'
+            },
+            {
+                title: 'Day 6: Departure',
+                description: 'Breakfast at the hotel; check out at 09:00 am. Transfer to Kutaisi - one of the most important political and cultural center of Georgia. Here we will visit - Gelati monastery (UNESCO sight), Bagrati church, Colchins fountain, Fruit Market. Later, we will continue to Tskaltubo and visit the beautiful natural monument - Cave of Prometheus. More then 1 km long cave is full of stalactites, stalagmites, underground river and halls. we will transfer to the airport.<br/><b>Departure flight to Abu Dhabi</b>'
+            }
+        ]
+    }
+]
